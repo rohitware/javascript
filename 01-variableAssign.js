e5511b45ca73b9b2651b1bee951daf33e752ac42
rohitware <41794103+rohitware@users.noreply.github.com>
2025-02-12 09:19:14 +0530

    01-variableAssignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,36 @@
+var myFullName;
+console.log("Full name value before initialization:", myFullName);
+
+myFullName = "Rohit Ware";
+console.log("Full name after initialization:", myFullName);
+
+myFullName = "Rohit Bhimrao Ware";
+console.log("Full name after update:", myFullName);
+
+
+var myLove = "I love only js";
+console.log( myLove);
+
+
+var firstName= "Rohit";
+var lastName = "Ware";
+var age= 35;
+var address = "Pune";
+var pinCode = 411046;
+var gender = "Male";
+var isMarried = true;
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pincode:", pinCode);
+console.log("Gender:", gender);
+console.log("is Married?:", isMarried);
+
+
+
+
+
+
+
