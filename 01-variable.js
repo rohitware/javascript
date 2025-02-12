9f99a98a9639e6f4249380c23cb6091f32c57c4f
rohitware <41794103+rohitware@users.noreply.github.com>
2025-02-12 08:41:16 +0530

    Create 01-variable.js
    variables in javadcript

---
+++ 01-variable.js
@@ -0,0 +1,8 @@
+var age; // Variable declaration
+console.log(age);  // undefined
+age = "22"; // Variable initialization
+console.log(age);
+console.log("AGE: ", age);
+
+var presentCity = "Pune"; // Variable declaration and initialization in the same line
+console.log("My Present City: ", presentCity);
