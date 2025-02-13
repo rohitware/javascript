be33ff4a37e8cffa037c2ba4d669a9cf1f2ca782
rohitware <41794103+rohitware@users.noreply.github.com>
2025-02-13 17:26:32 +0530

    Create 02-typeOf.js
    Variables and It's data types

---
+++ 02-typeOf.js
@@ -0,0 +1,21 @@
+var bankName;
+console.log(`value of bankName before initialization is ${bankName} and data type of it is: ${typeof (bankName)}`);
+
+bankName = "SBI Bank";
+console.log(`value of bankName after initialization is ${bankName} and data type of it is: ${typeof (bankName)}`);
+
+
+var marks = '90%';
+console.log(`data type of marks is: ${typeof (marks)} and value ot it is: ${marks}`);
+
+var isWorking;
+console.log(`value of isWorking before initialization is: ${isWorking} and data taype of it is: ${typeof (isWorking)}`);
+
+var isWorking = true;
+console.log(`value of isWorking after initialization is: ${isWorking} and data taype of it is: ${typeof (isWorking)}`);
+
+var totalCount= "Hundred and Seven";
+console.log(`value of total count is: ${totalCount} and the data type of it is: ${typeof(totalCount)}`);
+
+totalCount = 107;
+console.log(`Updated value of total count is: ${totalCount} and the data type of it is: ${typeof(totalCount)}`);
