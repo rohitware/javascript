5cfac0d82ac5d2cf40f0f8c869369399e463d8a4
rohitware <41794103+rohitware@users.noreply.github.com>
2025-02-17 09:06:14 +0530

    NaN

---
+++ 04-NaN.js
@@ -0,0 +1,12 @@
+var result = 0 / 0;
+console.log(result); // NaN ---> Not a Number
+console.log("==========");
+
+var n1;
+console.log(n1 + 10);
+
+console.log(`=== String to number conversion ===`);
+
+var str = "JavaScript";
+var result = +str;
+console.log(result);
