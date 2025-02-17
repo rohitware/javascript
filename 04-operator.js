0110d6e370e3758e032138fe835366527011d82a
rohitware <41794103+rohitware@users.noreply.github.com>
2025-02-17 09:06:39 +0530

    Operators

---
+++ 04-operator.js
@@ -0,0 +1,14 @@
+function isEven(num) {
+    console.log(`====== Ternary Operator ======`);
+    var result = num % 2 == 0 ? "EVEN" : "ODD";
+    console.log(`Given number ${num} is ${result}`);
+}
+isEven(5);
+isEven(10);
+
+console.log(`==== Number is Positive or Negative =====`);
+function isPositive(num) {
+    var result = num > 0 ? "Positive" : "Negative";
+    console.log(`Given number ${num} is ${result}`);
+}
+isPositive(7);
